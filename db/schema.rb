cd5d0a05a87c083bb6132a192755ed04cf70a27c
mlf <micafinn@gmail.com>
2017-03-31 13:22:02 +0200

    Generate and migrate items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331102035) do
+ActiveRecord::Schema.define(version: 20170331112103) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_todo_items_on_todo_id"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
