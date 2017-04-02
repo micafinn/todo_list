f5ee5d7791d7695fcf20b957f69aca337cb82496
mlf <micafinn@gmail.com>
2017-04-02 17:05:09 +0200

    Create destroy action for todo_items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,6 +6,16 @@ class TodoItemsController < ApplicationController
     redirect_to @todo
   end
 
+  def destroy
+    @todo_item = @todo.todo_items.find(params[:id])
+    if @todo.destroy
+      flash[:success] = "Todo List item was deleted."
+    else
+      flash[:error] = "Todo List item could not be deleted."
+    end
+    redirect_to @todo
+  end
+
   private
 
   def set_todo
