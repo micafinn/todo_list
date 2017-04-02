69306e744b9871ea411f7c3814e3c3bc7582a785
mlf <micafinn@gmail.com>
2017-04-02 17:10:13 +0200

    Add a completed_at column to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331112103) do
+ActiveRecord::Schema.define(version: 20170402150855) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
     t.integer  "todo_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
     t.index ["todo_id"], name: "index_todo_items_on_todo_id"
   end
 
