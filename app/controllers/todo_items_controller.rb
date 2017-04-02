fe3cc2b6cb30b061e1be8f4b57010273b537b32c
mlf <micafinn@gmail.com>
2017-04-02 16:51:01 +0200

    Create partials

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,18 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo
+  def create
+    @todo_item = @todo.todo_items.create(todo_item_params)
+
+    redirect_to @todo
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:todo_id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
 end
